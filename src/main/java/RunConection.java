be6458112e7e352002213d6d8c3cfcd7bd2aa1c5
ignaciojnr <38479182+ignaciojnr@users.noreply.github.com>
2019-09-04 17:03:23 -0400

    fix

--- src/main/java/RunConection.java
+++ src/main/java/RunConection.java
@@ -41,7 +41,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * paralel process conection
+ * parallelizable connection processing
  * @author Ignacio Rivera
  * @version 0.0.1
  */
